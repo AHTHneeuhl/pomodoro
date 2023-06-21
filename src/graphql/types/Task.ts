a7ffa85f48b4b9dc3f02935cc17f5e6dac547df0
Shubham Patrick <ahthneeuhl@gmail.com>
2023-06-21 21:16:14 +0530

    feat: create Tasks, mutation and component

--- src/graphql/types/Task.ts
+++ src/graphql/types/Task.ts
@@ -8,3 +8,30 @@ builder.prismaObject("Task", {
     dueDate: t.exposeString("dueDate"),
   }),
 });
+
+builder.mutationField("createTask", (t) =>
+  t.prismaField({
+    type: "Task",
+    args: {
+      title: t.arg.string({ required: true }),
+      description: t.arg.string({ required: true }),
+      dueDate: t.arg.string({ required: true }),
+    },
+    resolve: async (query, _parent, args, ctx) => {
+      const { title, description, dueDate } = args;
+
+      if (!(await ctx).user) {
+        throw new Error("You have to be logged in to perform this action");
+      }
+
+      return prisma.task.create({
+        ...query,
+        data: {
+          title,
+          description,
+          dueDate,
+        },
+      });
+    },
+  })
+);
