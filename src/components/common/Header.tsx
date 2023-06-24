0fe10e4901b5376dca71de56bc61e4bcb8ffd898
Shubham Patrick <ahthneeuhl@gmail.com>
2023-06-24 09:49:51 +0530

    feat: create header and timer with 25 mins components

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useUser } from "@auth0/nextjs-auth0/client";
-import { Box } from "grommet";
+import { Box, Heading } from "grommet";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,40 +9,31 @@ const Header = () => {
   const { user } = useUser();
 
   return (
-    <header>
-      <Box>
-        <Link href="/">
-          <svg
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
-            ></path>
-          </svg>
-        </Link>
-        <nav>
-          {user ? (
-            <Box className="flex items-center space-x-5">
-              <Link href="/api/auth/logout">Logout</Link>
-              <Image
-                alt="profile"
-                src={user.picture ? user.picture : ""}
-                width={32}
-                height={32}
-              />
-            </Box>
-          ) : (
-            <Link href="/api/auth/login">Login</Link>
-          )}
-        </nav>
-      </Box>
-    </header>
+    <Box
+      direction="row"
+      align="center"
+      justify="between"
+      pad={{ horizontal: "medium" }}
+    >
+      <Heading size="small" margin="none">
+        <Link href="/">Pomodoro</Link>
+      </Heading>
+      <nav>
+        {user ? (
+          <Box className="flex items-center space-x-5">
+            <Link href="/api/auth/logout">Logout</Link>
+            <Image
+              alt="profile"
+              src={user.picture ? user.picture : ""}
+              width={32}
+              height={32}
+            />
+          </Box>
+        ) : (
+          <Link href="/api/auth/login">Login</Link>
+        )}
+      </nav>
+    </Box>
   );
 };
 
--- src/components/pomodoro/Timer.tsx
+++ src/components/pomodoro/Timer.tsx
@@ -1,37 +1,98 @@
 "use client";
 
-import { Box, Meter } from "grommet";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Box, Heading } from "grommet";
+
+interface StyledProgressProps {
+  width: number;
+}
+
+const StyledMeterContainer = styled.div`
+  position: relative;
+  width: 250px; /* Adjust the width as needed */
+  height: 250px; /* Adjust the height as needed */
+  border-radius: 50%;
+  background-color: #fcfcfc; /* Adjust the background color as needed */
+  overflow: hidden;
+`;
+
+const StyledProgressBorder = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 24px solid #f1f1f1; /* Adjust the border width and color as needed */
+  border-radius: 50%;
+  box-sizing: border-box;
+`;
+
+const StyledProgressInner = styled.div<StyledProgressProps>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  border: 24px solid #7d4cdb; /* Adjust the border width and color as needed */
+  clip-path: ${({ width }) => `polygon(
+    50% 50%,
+    50% 0%,
+    calc(50% + ${Math.sin((1 - width / 100) * 2 * Math.PI) * 50}%)
+      calc(50% - ${Math.cos((1 - width / 100) * 2 * Math.PI) * 50}%),
+    50% 50%
+  )`};
+  transition: clip-path 0.5s ease-in-out;
+  box-sizing: border-box;
+`;
+
+const StyledProgressContent = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
 
 const Timer: React.FC = () => {
-  const [meterValue, setMeterValue] = useState(25);
+  const [meterValue, setMeterValue] = useState(0);
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
     if (meterValue < 100) {
-      const timer = setTimeout(() => {
-        setMeterValue((meterValue) => meterValue + 1);
+      const timer = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+        } else {
+          if (minutes === 0) {
+            clearInterval(timer);
+            // Timer has finished, you can add your logic here
+          } else {
+            setMinutes(minutes - 1);
+            setSeconds(59);
+          }
+        }
+        setMeterValue((meterValue) => meterValue - (1 / 1500) * 100);
       }, 1000);
 
       return () => {
-        clearTimeout(timer);
+        clearInterval(timer);
       };
     }
-  }, [meterValue]);
+  }, [meterValue, minutes, seconds]);
 
   return (
     <Box align="center">
-      <Meter
-        type="circle"
-        values={[
-          {
-            value: meterValue,
-            color: "graph-0",
-            highlight: true,
-            label: "string",
-            onClick: () => {},
-          },
-        ]}
-      />
+      <StyledMeterContainer>
+        <StyledProgressBorder />
+        <StyledProgressInner width={meterValue} />
+        <StyledProgressContent>
+          <Heading size="medium">
+            {`${minutes}:${seconds.toString().padStart(2, "0")}`}
+          </Heading>
+        </StyledProgressContent>
+      </StyledMeterContainer>
     </Box>
   );
 };
